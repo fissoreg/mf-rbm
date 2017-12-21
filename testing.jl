6121eb9153c0783027a467e5b33cea0bfd252877
Giancarlo Fissore <giancarlo.fissore@gmail.com>
2017-12-21 11:48:05 +0100

    Minor correction.

--- testing.jl
+++ testing.jl
@@ -5,7 +5,7 @@ include("../MNIST_utils/src/MNIST_utils.jl")
 
 X, y = testdata()
 
-m_v, m_h = get_fp(rbm, X[:,1:100], tap_v_fp, tap_h_fp)
+m_v, m_h = fp_iter(rbm, X[:,1:100], tap_v_fp, tap_h_fp; max_iter=1000)
 
 s = samplesToImg(m_v);
 
